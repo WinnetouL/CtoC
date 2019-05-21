a47335cbd5ea043789a51969bfa5f6861d7de598
unknown <T3>
2019-05-21 19:38:38 +0200

    asyncio: adding my results from vacationing

--- test_Cli1_CtoC.py
+++ test_Cli1_CtoC.py
@@ -1,11 +1,6 @@
-# to client-2: receive msg; send msg; receive msg
-# within defined functions
-# just one socket in use
-# modular structure
-
-import socket
+import asyncio
 import time
-import sys
+import socket
 
 
 
@@ -22,27 +17,26 @@ class client1Class:
         self.sock.bind((client1Class.TCP_IP, client1Class.TCP_PORT))
         self.sock.listen(1)
         self.conn, self.addr = self.sock.accept()       # the accept funtion returns 2 values, which get assigned to variable
+        print("---- Client 1 active - waiting for connections ----\n")
+        asyncio.run(main())
 
-
-    def main(self):
-        print("---- Client 1 active ----")
- ###### NEW ######
-        a = self.sock.getsockname()
-        print('Connection from: client-1qqqqqqqqqqqqqqqq ', a[0], 'to CLIENT-2222222222222: ', self.addr[0])      # instead of "self.addr" I could just use TCP_IP
-        self.receiveCli1()
-        self.sendCli1()
- ###### NEW ###### 
+    async def main(self):
         try:
-            self.receiveCli1()
+            print('Connection from: client-1 ', socket.gethostbyname(socket.gethostname()), ' to CLIENT-2: ', self.addr[0])      # instead of "self.addr" I could just use TCP_IP
+            
+            task0 = asyncio.create_task(self.receiveCli1())
+            task1 = asyncio.create_task(self.sendCli1())
+
+            await task0
+            await task1
+            
         except ConnectionResetError as e:
-            print("TROLLLLLLLLLLLLLLLLL", e)
-            time.sleep(3)
-            print("return to main")
+            print("Client-2 closed the window\n", "OS-Error:", e, "\nApplication restarted")
             self.__init__()
             self.main()
-    
 
-    def receiveCli1(self):
+
+    async def receiveCli1(self):
 
         while True:
             fullClient2Msg = ''
@@ -55,7 +49,7 @@ class client1Class:
                     print(f"First 10 characters of Client-2's message: {msg[:client1Class.HEADERSIZE]}")       # printing out the first 10 characters (Headersize) of the message
                     msgLen = int(msg[:client1Class.HEADERSIZE])      # append everything of the first 10 characters (Headersize) to 'msgLen'. In our case it is always just the number of the length and blanks nothing else, so e.g.: 22
                     newClient2Msg = False
-                
+
                 fullClient2Msg += msg.decode("utf-8")        # decode the chunks of the received msg by given transformation format and append 16 characters each turn of the while loop to the full msg variable ('fullClient2Msg')
 
                 if len(fullClient2Msg)-client1Class.HEADERSIZE == msgLen:     # this part is only going to be passed if the (length of 'fullClient2Msg' (1.round=16, 2.round=32 (next 16 'or less'))) - ('Headersize' (10 characters)) equals the determined ('msgLen' (22))
@@ -64,7 +58,7 @@ class client1Class:
             break
 
 
-    def sendCli1(self):
+    async def sendCli1(self):
 
         while True:
             msg = "Welcome from CLIENT-1!"
@@ -76,8 +70,295 @@ class client1Class:
 
 client1Object = client1Class()
 client1Object.main()
+# asyncio.run(main())
 
 
 
 
-# self.conn.close()???
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+    async def say_after(delay, what):
+        await asyncio.sleep(delay)
+        print(what)
+
+    async def main():
+        task1 = asyncio.create_task(
+            say_after(1, 'hello'))
+
+        task2 = asyncio.create_task(
+            say_after(2, 'world'))
+
+        print(f"started at {time.strftime('%X')}")
+
+        # Wait until both tasks are completed (should take
+        # around 2 seconds.)
+        await task1
+        await task2
+
+        print(f"finished at {time.strftime('%X')}")
+
+    asyncio.run(main())
+
+
+
+
+
+
+
+async def main():
+    print(f"started at {time.strftime('%X')}")
+
+    await say_after(1, 'hello')
+    await say_after(2, 'world')
+
+    print(f"finished at {time.strftime('%X')}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# https://asyncio.readthedocs.io/en/latest/tcp_echo.html
+import asyncio
+
+async def handle_echo(reader, writer):
+    data = await reader.read(100)
+    message = data.decode()
+    addr = writer.get_extra_info('peername')
+    print(writer.get_extra_info('sockname'))
+
+    
+    print("Received %r from %r" % (message, addr))
+
+    # print("Send: %r" % message)
+    new = input('newMsg ')
+    print("Send: %r" % new)
+ 
+    writer.write(bytes(new, 'utf-8'))
+    await writer.drain()
+
+    print("Close the client socket")
+    writer.close()
+
+loop = asyncio.get_event_loop()
+coro = asyncio.start_server(handle_echo, '127.0.0.1', 8888, loop=loop)
+server = loop.run_until_complete(coro)
+
+# Serve requests until Ctrl+C is pressed
+print('Serving on {}'.format(server.sockets[0].getsockname()))
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
+    
+# Close the server
+server.close()
+loop.run_until_complete(server.wait_closed())
+loop.close()
+
+
+
+
+
+
+
+
+
+
+
+
+_________________________________________________________
+
+
+
+
+
+
+
+
+
+import asyncio
+import socket
+import errno
+
+
+def pair(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=0):
+    """Emulate the Unix socketpair() function on Windows."""
+    # https://gist.github.com/geertj/4325783
+    # We create a connected TCP socket. Note the trick with setblocking(0)
+    # that prevents us from having to create a thread.
+    lsock = socket.socket(family, type, proto)
+    lsock.bind(('127.0.0.1', 1234)) 
+    lsock.listen(1)
+    #addr, port = lsock.getsockname()
+    addr = '127.0.0.1'
+    port = 1234
+    csock = socket.socket(family, type, proto)
+    csock.setblocking(0)
+    try:
+        csock.connect((addr, port))
+    except socket.error as e:
+        if e.errno != errno.WSAEWOULDBLOCK:
+            raise
+    csock.setblocking(0)
+    lsock.close()
+    
+    print(csock)
+    return (csock)
+
+
+async def wait_for_data():
+    # Get a reference to the current event loop because
+    # we want to access low-level APIs.
+    loop = asyncio.get_running_loop()
+
+    # Create a pair of connected sockets.
+    rsock, wsock = socket.socketpair()
+    sockets = pair()
+    print('tada', sockets)
+    
+    # Register the open socket to wait for data.
+    reader, writer = await asyncio.open_connection(sock=rsock)
+
+    # Simulate the reception of data from the network
+    loop.call_soon(wsock.send, 'abc'.encode())
+
+    # Wait for data
+    data = await reader.read(100)
+
+    # Got data, we are done: close the socket
+    print("Received:", data.decode())
+    writer.close()
+
+    # Close the second socket
+    wsock.close()
+
+asyncio.run(wait_for_data())
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+import asyncio
+import socket
+import errno
+
+
+def pair(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=0):
+    """Emulate the Unix socketpair() function on Windows."""
+    # https://gist.github.com/geertj/4325783
+    # We create a connected TCP socket. Note the trick with setblocking(0)
+    # that prevents us from having to create a thread.
+    lsock = socket.socket(family, type, proto)
+    lsock.bind(('127.0.0.1', 1234)) 
+    lsock.listen(1)
+    #addr, port = lsock.getsockname()
+    addr = '127.0.0.1'
+    port = 1234
+    csock = socket.socket(family, type, proto)
+   # csock.setblocking(0) -> not needed?
+    csock.setblocking(0)
+    try:
+        csock.connect((addr, port))
+    except socket.error as e:
+        if e.errno != errno.WSAEWOULDBLOCK:
+            raise
+    ssock, addr = lsock.accept()
+    print(ssock)
+    print(addr)
+   # csock.setblocking(1) -> not needed?
+    csock.setblocking(1)
+    lsock.close()
+    print(ssock)
+    print(csock)
+    return (ssock, csock)
+
+
+async def wait_for_data():
+    # Get a reference to the current event loop because
+    # we want to access low-level APIs.
+    loop = asyncio.get_running_loop()
+
+    # Create a pair of connected sockets.
+    #rsock, wsock = socket.socketpair()
+    sockets = pair()
+    print(sockets[0])
+    print(sockets[1])
+    rsock = sockets[0]
+    wsock = sockets[1]
+
+    # Register the open socket to wait for data.
+    reader, writer = await asyncio.open_connection(sock=rsock)
+
+    # Simulate the reception of data from the network
+    loop.call_soon(wsock.send, 'abc'.encode())
+
+    # Wait for data
+    data = await reader.read(100)
+
+    # Got data, we are done: close the socket
+    print("Received:", data.decode())
+    writer.close()
+
+    # Close the second socket
+    wsock.close()
+
+asyncio.run(wait_for_data())
+
+
+'''
--- test_Cli2_CtoC.py
+++ test_Cli2_CtoC.py
@@ -1,79 +1,136 @@
-# to client-1: send msg; receive msg; send msg
-# within defined functions
-# just one socket in use
-# modular structure
+import asyncio
+import time
 
+
+
+#class test:
+ #   async def say_after(self, delay, what):
+async def say_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
+
+#async def main2(self):
+async def main():
+ 
+    print('crazy')
+
+    task0 = asyncio.create_task(
+        #self.say_after(1, 'hello'))
+        say_after(1, 'hello'))
+
+    task1 = asyncio.create_task(
+        #self.say_after(2, 'world'))
+        say_after(2, 'world'))
+
+    print(f"started at {time.strftime('%X')}")
+
+    # Wait until both tasks are completed (should take
+    # around 2 seconds.)
+    
+    await task0
+    await task1
+
+
+    print(f"finished at {time.strftime('%X')}")
+
+    
+asyncio.run(main())
+
+
+#testobjec = test()
+#testobjec.main(asyncio.run(main()))
+
+
+
+
+'''
+import asyncio
+
+
+async def tcp_echo_client(message, loop):
+    reader, writer = await asyncio.open_connection('127.0.0.1', 8888, loop=loop)
+
+    message = input('new: ')
+    print('Send: %r' % message)
+    writer.write(message.encode())
+
+    data = await reader.read(100)
+    print('Received: %r' % data.decode())
+
+    print('Close the socket')
+    writer.close()
+
+
+
+#message = 'Hello World!'
+message = ''
+loop = asyncio.get_event_loop()
+loop.run_until_complete(tcp_echo_client(message, loop))
+loop.close()
+
+
+
+
+
+
+
+______________________________________________
+
+
+
+
+
+
+
+
+
+
+
+import asyncio
 import socket
 import errno
-import sys
 
 
-
-class client2Class:
-    # class variabels:
-    TCP_IP = socket.gethostbyname(socket.gethostname())
-#    TCP_IP = '192.168.2.134'
-    TCP_PORT = 1234
-    HEADERSIZE = 10
-
-    # connect to the default port
-    def __init__(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       # SOCK_STREAM -> TCP
-        self.sock.connect((client2Class.TCP_IP, client2Class.TCP_PORT))       # connect to client socket
-
-
-    def main(self):
-        print("---- Client 2 active ----")
-        print('Connection from client-2: ', self.sock.getsockname(), 'to CLIENT-1: ', client2Class.TCP_IP)
-        msg = "Welcome from CLIENT-2!"
-        self.sendCli2(msg)
-        self.recvCli2()
-        msg = input("Second msg to client-1?: ")
-
-        self.sendCli2(msg)
-            
-       # except errno.WSAECONNABORTED:
-        #    print('Connection is closed')
-
-    def sendCli2(self, msg):
- ###### NEW ######
-        try:
-            while True:
-                msg = f"{len(msg):<{client2Class.HEADERSIZE}}" + msg     # rebuild msg: counting length of msg and append the length number in front of the msg within the defined headersize
-                self.sock.send(bytes(msg, "utf-8"))     # send msg in given transformation format
-                break
-        except ConnectionResetError as e:
-            print("TROLLLLLLLLLLLLLLLLL", e)
-    
-    
-    def recvCli2(self):
-
-        while True:
-            fullClient1Msg = ''
-            newClient1Msg = True
-
-            while True:
-                msg = self.sock.recv(16)
-
-                if newClient1Msg == True:
-                    print(f"First 10 characters of client-1's message: {msg[:client2Class.HEADERSIZE]}")
-                    msgLen = int(msg[:client2Class.HEADERSIZE])
-                    newClient1Msg = False
-
-                fullClient1Msg += msg.decode("utf-8")
-
-                if len(fullClient1Msg)-client2Class.HEADERSIZE == msgLen:
-                    print("Client-1's message received: ", fullClient1Msg[client2Class.HEADERSIZE:])
-                    break
-            break
+def pair(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=0):
+    """Emulate the Unix socketpair() function on Windows."""
+    # https://gist.github.com/geertj/4325783
+    # We create a connected TCP socket. Note the trick with setblocking(0)
+    # that prevents us from having to create a thread.
+    lsock = socket.socket(family, type, proto)
+    lsock.bind(('127.0.0.1', 1234)) 
+    lsock.listen(1)
+    ssock, addr = lsock.accept()
+    print(addr)
+   # csock.setblocking(1) -> not needed?
+    lsock.close()
+    return (ssock)
 
 
+async def wait_for_data():
+    # Get a reference to the current event loop because
+    # we want to access low-level APIs.
+    loop = asyncio.get_running_loop()
 
-client2Object = client2Class()
-client2Object.main()
+    # Create a pair of connected sockets.
+    rsock, wsock = socket.socketpair()
+    sockets = pair()
+    print('uff', sockets)
 
+    # Register the open socket to wait for data.
+    reader, writer = await asyncio.open_connection(sock=rsock)
 
+    # Simulate the reception of data from the network
+    loop.call_soon(wsock.send, 'abc'.encode())
 
+    # Wait for data
+    data = await reader.read(100)
 
-# self.s.close()???
-# later: getsockname() and getpeername()
+    # Got data, we are done: close the socket
+    print("Received:", data.decode())
+    writer.close()
+
+    # Close the second socket
+    wsock.close()
+
+asyncio.run(wait_for_data())
+'''
