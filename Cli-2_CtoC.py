01f9767c56a580db9b27a264ec417b141bb2bc2a
WinnetouL <50875993+WinnetouL@users.noreply.github.com>
2019-06-16 00:41:38 +0200

    ver 6: error catching works properly; doesn't spawn a random amount of threads anymore (always 3)

--- Cli-2_CtoC.py
+++ Cli-2_CtoC.py
@@ -2,13 +2,14 @@
 # within defined functions
 # just one socket in use
 # modular structure
-# ?????? prevent application from crashing due an expected 'ConnectionResetError' (connection handling) and quitting
+# prevent application from crashing due an expected 'ConnectionResetError' (connection handling) and quitting
 
+import sys
+import time
 import socket
 import threading
 
-import time
-import sys
+
 
 class client2Class:
     # class variabels:
@@ -16,45 +17,37 @@ class client2Class:
 #    TCP_IP = '192.168.2.134'
     TCP_PORT = 1234
     HEADERSIZE = 10
-    stopThreads = False
 
     # connect to the default port
     def __init__(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       # SOCK_STREAM -> TCP
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       # AF_INET = IPv4 | SOCK_STREAM = TCP
         self.sock.connect((client2Class.TCP_IP, client2Class.TCP_PORT))       # connect to client socket
         print("---- Client 2 active ----\n")
 
 
     def main(self):
-            print('Connection from client-2: ', socket.gethostbyname(socket.gethostname()), ' to CLIENT-1: ', client2Class.TCP_IP)
-            # msg = "Welcome from CLIENT-2!"
-            t0 = threading.Thread(target = self.sendCli2)
-            t1 = threading.Thread(target = self.recvCli2)
-            t0.daemon = True        # mark these functiones as daemon threads, which are parts of the main thread (main > daemon).
-            t1.daemon = True        # This enables the possibility to stop the main thread with cmd + c and the other threads will stop as well, because they are daemons. Otherwise the started threads would continuously run.
-            t0.start()
-            t1.start()
+        print('Connection from client-2: ', socket.gethostbyname(socket.gethostname()), ' to CLIENT-1: ', client2Class.TCP_IP)
+        
+        t0 = threading.Thread(target = self.sendCli2)
+        t1 = threading.Thread(target = self.recvCli2)
+        t0.daemon = True        # mark these functiones as daemon threads, which are parts of the main thread (main > daemon).
+        t1.daemon = True        # This enables the possibility to stop the main thread with cmd + c and the other threads will stop as well, because they are daemons. Otherwise the started threads would continuously run.
+        t0.start()
+        t1.start()
 
+        while True:
             a = threading.active_count()
-            print(a)
-            while True:
-                print("THIS", client2Object.stopThreads)
-                if a != 3:
-                    print("nr", a)
-                    print("shutting down")
-                    sys.exit(0)
-                else:
-                    a = threading.active_count()
-                    # b = threading.enumerate()
-                    print("nr", a)
-                    # print(b)
-                    time.sleep(8)
-            # msg = input("Second msg to client-1?: ")
-            # self.sendCli2(msg)
-
+            if a != 3:
+                a = threading.active_count()
+                print("Threads alive: ", a)
+                print("Shutting down!")
+                sys.exit(0)
+            else:
+                time.sleep(2)
+                pass
+                
 
     def sendCli2(self):
-        
         while True:
             msg = input("Second msg to client-1?: ")
             msg = f"{len(msg):<{client2Class.HEADERSIZE}}" + msg     # rebuild msg: counting length of msg and append the length number in front of the msg within the defined headersize
@@ -79,10 +72,8 @@ class client2Class:
                     if len(fullClient1Msg)-client2Class.HEADERSIZE == msgLen:
                         print("Client-1's message received: ", fullClient1Msg[client2Class.HEADERSIZE:])
                         break
-
         except ConnectionResetError as e:
             print("Client-1 closed the window\n", "OS-Error:", e, "\nApplication quitted")
-            client2Object.stopThreads = True
             
             
 
