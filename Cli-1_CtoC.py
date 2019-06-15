faf0139c4d8bc9d328005b0ede1ea75b8c0e80a3
WinnetouL <50875993+WinnetouL@users.noreply.github.com>
2019-06-16 00:27:03 +0200

    ver 6: code works as expected, but doesn't run forever anymore

--- Cli-1_CtoC.py
+++ Cli-1_CtoC.py
@@ -40,32 +40,28 @@ class client1Class:
 
 
     def receiveCli1(self):
+        fullClient2Msg = ''
+        newClient2Msg = True
 
         while True:
-            fullClient2Msg = ''
-            newClient2Msg = True
+            msg = self.conn.recv(16)
 
-            while True:
-                msg = self.conn.recv(16)
+            if newClient2Msg == True:        # the if part will be passed just once to figure out how long the msg will be, because newClientRespond is going to be set to false
+                print(f"First 10 characters of Client-2's message: {msg[:client1Class.HEADERSIZE]}")       # printing out the first 10 characters (Headersize) of the message
+                msgLen = int(msg[:client1Class.HEADERSIZE])      # append everything of the first 10 characters (Headersize) to 'msgLen'. In our case it is always just the number of the length and blanks nothing else, so e.g.: 22
+                newClient2Msg = False
+            
+            fullClient2Msg += msg.decode("utf-8")        # decode the chunks of the received msg by given transformation format and append 16 characters each turn of the while loop to the full msg variable ('fullClient2Msg')
 
-                if newClient2Msg == True:        # the if part will be passed just once to figure out how long the msg will be, because newClientRespond is going to be set to false
-                    print(f"First 10 characters of Client-2's message: {msg[:client1Class.HEADERSIZE]}")       # printing out the first 10 characters (Headersize) of the message
-                    msgLen = int(msg[:client1Class.HEADERSIZE])      # append everything of the first 10 characters (Headersize) to 'msgLen'. In our case it is always just the number of the length and blanks nothing else, so e.g.: 22
-                    newClient2Msg = False
-                
-                fullClient2Msg += msg.decode("utf-8")        # decode the chunks of the received msg by given transformation format and append 16 characters each turn of the while loop to the full msg variable ('fullClient2Msg')
-
-                if len(fullClient2Msg)-client1Class.HEADERSIZE == msgLen:     # this part is only going to be passed if the (length of 'fullClient2Msg' (1.round=16, 2.round=32 (next 16 'or less'))) - ('Headersize' (10 characters)) equals the determined ('msgLen' (22))
-                    print("Client-2's message received: ", fullClient2Msg[client1Class.HEADERSIZE:])        # if this is true full message is received. Printing everything out continuing after the 10 characters (Headersize)
-                    break
+            if len(fullClient2Msg)-client1Class.HEADERSIZE == msgLen:     # this part is only going to be passed if the (length of 'fullClient2Msg' (1.round=16, 2.round=32 (next 16 'or less'))) - ('Headersize' (10 characters)) equals the determined ('msgLen' (22))
+                print("Client-2's message received: ", fullClient2Msg[client1Class.HEADERSIZE:])        # if this is true full message is received. Printing everything out continuing after the 10 characters (Headersize)
+                break
 
 
     def sendCli1(self):
-
-        while True:
-            msg = "Welcome from CLIENT-1!"
-            msg = f"{len(msg):<{client1Class.HEADERSIZE}}" + msg
-            self.conn.send(bytes(msg, "utf-8"))
+        msg = "Welcome from CLIENT-1!"
+        msg = f"{len(msg):<{client1Class.HEADERSIZE}}" + msg
+        self.conn.send(bytes(msg, "utf-8"))
 
 
 
