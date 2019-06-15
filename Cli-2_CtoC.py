5af31e145955cb68e650c0dfaa5a4543aa12ec2c
WinnetouL <50875993+WinnetouL@users.noreply.github.com>
2019-06-16 00:33:52 +0200

    ver 6: starting to implement asyncio

--- Cli-2_CtoC.py
+++ Cli-2_CtoC.py
@@ -1,75 +1,111 @@
-# to client-1: send msg; receive msg; send msg
-# within defined functions
-# just one socket in use
-# modular structure
-# prevent application from crashing due an expected 'ConnectionResetError' (connection handling) and quitting
-
+import asyncio
 import socket
 
 
+class MyProtocol(asyncio.Protocol):
 
-class client2Class:
-    # class variabels:
-    TCP_IP = socket.gethostbyname(socket.gethostname())
-#    TCP_IP = '192.168.2.134'
-    TCP_PORT = 1234
-    HEADERSIZE = 10
+    def __init__(self, loop):
+        #self.transport = None
+        self.on_con_lost = loop.create_future()
 
-    # connect to the default port
-    def __init__(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       # SOCK_STREAM -> TCP
-        self.sock.connect((client2Class.TCP_IP, client2Class.TCP_PORT))       # connect to client socket
-        print("---- Client 2 active ----\n")
+    #def connection_made(self, transport):
+    #    self.transport = transport
 
+    def data_received(self, data):
+        print("Received:", data.decode())
 
-    def main(self):
-        try:
-            print('Connection from client-2: ', socket.gethostbyname(socket.gethostname()), ' to CLIENT-1: ', client2Class.TCP_IP)
-            msg = "Welcome from CLIENT-2!"
-            self.sendCli2(msg)
-            self.recvCli2()
-            msg = input("Second msg to client-1?: ")
-            self.sendCli2(msg)
-        except ConnectionResetError as e:
-            print("Client-1 closed the window\n", "OS-Error:", e, "\nApplication quitted")
+        # We are done: close the transport;
+        # connection_lost() will be called automatically.
+        self.transport.close()
+
+    def connection_lost(self, exc):
+        # The socket has been closed
+        self.on_con_lost.set_result(True)
 
 
-    def sendCli2(self, msg):
+async def main():
+    # Get a reference to the event loop as we plan to use
+    # low-level APIs.
+    loop = asyncio.get_running_loop()
 
-        while True:
-            msg = f"{len(msg):<{client2Class.HEADERSIZE}}" + msg     # rebuild msg: counting length of msg and append the length number in front of the msg within the defined headersize
-            self.sock.send(bytes(msg, "utf-8"))     # send msg in given transformation format
-            break
+    # Create a pair of connected sockets
+    rsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print(rsock)
+    #print(wsock)
 
+    # Register the socket to wait for data.
+    transport, protocol = await loop.create_connection(
+        lambda: MyProtocol(loop), sock=rsock)
+   
 
-    def recvCli2(self):
+    # Simulate the reception of data from the network.
+    #loop.call_soon(wsock.send, 'abc'.encode())
 
-        while True:
-            fullClient1Msg = ''
-            newClient1Msg = True
+    try:
+        await protocol.on_con_lost
+    finally:
+        transport.close()
+        #wsock.close()
 
-            while True:
-                msg = self.sock.recv(16)
-
-                if newClient1Msg == True:
-                    print(f"First 10 characters of client-1's message: {msg[:client2Class.HEADERSIZE]}")
-                    msgLen = int(msg[:client2Class.HEADERSIZE])
-                    newClient1Msg = False
-
-                fullClient1Msg += msg.decode("utf-8")
-
-                if len(fullClient1Msg)-client2Class.HEADERSIZE == msgLen:
-                    print("Client-1's message received: ", fullClient1Msg[client2Class.HEADERSIZE:])
-                    break
-            break
-
-
-
-client2Object = client2Class()
-client2Object.main()
+asyncio.run(main())
 
 
 
 
-# self.s.close()???
-# later: getsockname() and getpeername()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+import asyncio
+import socket
+class MyProtocol(asyncio.Protocol):
+    def __init__(self, loop):
+        self.transport = None
+        self.on_con_lost = loop.create_future()
+    def connection_made(self, transport):
+        self.transport = transport
+    def data_received(self, data):
+        print("Received:", data.decode())
+        # We are done: close the transport;
+        # connection_lost() will be called automatically.
+        self.transport.close()
+    def connection_lost(self, exc):
+        # The socket has been closed
+        self.on_con_lost.set_result(True)
+async def main():
+    # Get a reference to the event loop as we plan to use
+    # low-level APIs.
+    loop = asyncio.get_running_loop()
+    # Create a pair of connected sockets
+    rsock, wsock = socket.socketpair()
+    print(rsock)
+    print(wsock)
+    # Register the socket to wait for data.
+    transport, protocol = await loop.create_connection(
+        lambda: MyProtocol(loop), sock=rsock)
+   
+    # Simulate the reception of data from the network.
+    loop.call_soon(wsock.send, 'abc'.encode())
+    try:
+        await protocol.on_con_lost
+    finally:
+        transport.close()
+        wsock.close()
+asyncio.run(main())
+'''
