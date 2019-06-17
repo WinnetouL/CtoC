43ee514e7f94fc5bc88655862cceb0fd9d7f6c44
WinnetouL <50875993+WinnetouL@users.noreply.github.com>
2019-06-17 19:08:26 +0200

    ver 4: removed second socket -> only one socket in use | removed redundant code (calling the required functions from main)

--- Cli-1_CtoC.py
+++ Cli-1_CtoC.py
@@ -1,79 +1,71 @@
-# to client 2: send msg; receive msg; send msg
+# to client-2: receive msg; send msg; receive msg
 # within defined functions
+# just one socket in use
+# modular structure
 
 import socket
 
 
 
 class client1Class:
-    # class variabels: 
+    # class variabels:
+    TCP_IP = socket.gethostbyname(socket.gethostname())
+#    TCP_IP = '192.168.2.134'
+    TCP_PORT = 1234
     HEADERSIZE = 10
-    IP = socket.gethostbyname(socket.gethostname())     # IP ~ "192.168.2.111"
-    PORT = 1234
-    PORT2 = 1235
 
     # initalize default used port
-    def __init__(self):    
-        self.sock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       # SOCK_STREAM -> TCP
-        self.sock1.bind((client1Class.IP, client1Class.PORT))
-        self.sock1.listen(1)
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((client1Class.TCP_IP, client1Class.TCP_PORT))
+        self.sock.listen(1)
+        self.conn, self.addr = self.sock.accept()       # the accept funtion returns 2 values, which get assigned to variable
 
 
     def main(self):
         print("---- Client 1 active ----")
-        
+        print('Connection from: client-1 ', self.sock.getsockname(), 'to CLIENT-2: ', self.addr)      # instead of "self.addr" I could just use TCP_IP
+        self.receiveCli1()
+        self.sendCli1()
+        self.receiveCli1()
+
+
+    def receiveCli1(self):
+
         while True:
-            conn, address = self.sock1.accept()      # the accept funtion returns 2 values, which get assigned to variable
-            print(f"CLIENT 2 CONNECTED: {address} has been established!")
-            msg = "Welcome from client 1!"
-            msg = f"{len(msg):<{client1Class.HEADERSIZE}}" + msg     # rebuild msg: counting length of msg and append the length number in front of the msg within the defined headersize
-            conn.send(bytes(msg, "utf-8"))      # send msg in given transformation format      
-            self.client1Recv()
-
-            while True:     # calling sendSecondMsg() function over and over again
-                msg1 = input("send another msg to client 2: ")
-                self.sendSecondMsg(conn, msg1)
-
-
-    def client1Recv(self):
-        sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock2.connect((client1Class.IP, client1Class.PORT2))     	# connect to client socket
-        
-        while True:
-            fullClient2Respond = ''
-            newClient2Respond = True
+            fullClient2Msg = ''
+            newClient2Msg = True
 
             while True:
-                chunkClient2Respond = sock2.recv(16)
+                msg = self.conn.recv(16)
 
-                if newClient2Respond == True:        
-                    print(f"first 10 characters of message: {chunkClient2Respond[:client1Class.HEADERSIZE]}")
-                    respondLen = int(chunkClient2Respond[:client1Class.HEADERSIZE])
-                    newClient2Respond = False 
+                if newClient2Msg == True:        # the if part will be passed just once to figure out how long the msg will be, because newClientRespond is going to be set to false
+                    print(f"First 10 characters of Client-2's message: {msg[:client1Class.HEADERSIZE]}")       # printing out the first 10 characters (Headersize) of the message
+                    msgLen = int(msg[:client1Class.HEADERSIZE])      # append everything of the first 10 characters (Headersize) to 'msgLen'. In our case it is always just the number of the length and blanks nothing else, so e.g.: 22
+                    newClient2Msg = False
+                
+                fullClient2Msg += msg.decode("utf-8")        # decode the chunks of the received msg by given transformation format and append 16 characters each turn of the while loop to the full msg variable ('fullClient2Msg')
 
-                fullClient2Respond += chunkClient2Respond.decode("utf-8")
-
-                if len(fullClient2Respond)-client1Class.HEADERSIZE == respondLen:
-                    print("----Full message recv:----")
-                    print("from Client 2: " + fullClient2Respond[client1Class.HEADERSIZE:])
+                if len(fullClient2Msg)-client1Class.HEADERSIZE == msgLen:     # this part is only going to be passed if the (length of 'fullClient2Msg' (1.round=16, 2.round=32 (next 16 'or less'))) - ('Headersize' (10 characters)) equals the determined ('msgLen' (22))
+                    print("Client-2's message received: ", fullClient2Msg[client1Class.HEADERSIZE:])        # if this is true full message is received. Printing everything out continuing after the 10 characters (Headersize)
                     break
             break
-             
-
-    def sendSecondMsg(self, conn, msg1):
-        print("Going to be send: ", msg1)
-        msg1 = f"{len(msg1):<{client1Class.HEADERSIZE}}" + msg1
-        conn.send(bytes(msg1, "utf-8"))
 
 
-  
+    def sendCli1(self):
+
+        while True:
+            msg = "Welcome from CLIENT-1!"
+            msg = f"{len(msg):<{client1Class.HEADERSIZE}}" + msg
+            self.conn.send(bytes(msg, "utf-8"))
+            break
+
+
+
 client1Object = client1Class()
 client1Object.main()
 
 
 
 
-# keep in mind:
-# self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-# sock.close shutdown
-# sock2.setblocking(True)
+# self.conn.close()???
