ac6590231afb673c7e765ad994181fae9ea16900
WinnetouL <50875993+WinnetouL@users.noreply.github.com>
2019-06-17 20:58:46 +0200

    ver 6: nothing code related

--- Cli-2.py
+++ Cli-2.py
@@ -12,15 +12,14 @@ import threading
 
 class client2Class:
     # class variabels:
-    TCP_IP = socket.gethostbyname(socket.gethostname())
-    # TCP_IP = '192.168.2.134'
+    TCP_IP = socket.gethostbyname(socket.gethostname())                     # TCP_IP = 'XXX.XXX.XXX.XXX'
     TCP_PORT = 1234
     HEADERSIZE = 10
 
     # connect to the default port
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       # AF_INET = IPv4 | SOCK_STREAM = TCP
-        self.sock.connect((client2Class.TCP_IP, client2Class.TCP_PORT))       # connect to client socket
+        self.sock.connect((client2Class.TCP_IP, client2Class.TCP_PORT))     # connect to client socket
         print("---- Client 2 active ----\n")
 
 
@@ -29,8 +28,8 @@ class client2Class:
         
         t0 = threading.Thread(target = self.sendCli2)
         t1 = threading.Thread(target = self.recvCli2)
-        t0.daemon = True        # mark these functiones as daemon threads, which are parts of the main thread (main > daemon).
-        t1.daemon = True        # This enables the possibility to stop the main thread with cmd + c and the other threads will stop as well, because they are daemons. Otherwise the started threads would continuously run.
+        t0.daemon = True                                                    # mark these functiones as daemon threads, which are parts of the main thread (main > daemon).
+        t1.daemon = True                                                    # This enables the possibility to stop the main thread with cmd + c and the other threads will stop as well, because they are daemons. Otherwise the started threads would continuously run.
         t0.start()
         t1.start()
 
@@ -49,8 +48,8 @@ class client2Class:
     def sendCli2(self):
         while True:
             msg = input("Second msg to client-1?: ")
-            msg = f"{len(msg):<{client2Class.HEADERSIZE}}" + msg     # rebuild msg: counting length of msg and append the length number in front of the msg within the defined headersize
-            self.sock.send(bytes(msg, "utf-8"))     # send msg in given transformation format
+            msg = f"{len(msg):<{client2Class.HEADERSIZE}}" + msg            # rebuild msg: counting length of msg and append the length number in front of the msg within the defined headersize
+            self.sock.send(bytes(msg, "utf-8"))                             # send msg in given transformation format
         
 
     def recvCli2(self):
