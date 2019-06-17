a36884b06e45b4f54f1d98e1117115d4f6f0f7d9
WinnetouL <50875993+WinnetouL@users.noreply.github.com>
2019-06-17 19:06:52 +0200

    ver 2: everything within defined functiones

--- Cli-1_CtoC.py
+++ Cli-1_CtoC.py
@@ -1,38 +1,41 @@
-# to client 2: send messages
+# to client 1: send messages
+# within defined functions
 
 import socket
 
 
 
-HEADERSIZE = 10
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((socket.gethostname(), 1234))
-sock.listen(3)
-
-
-while True:
-    # double assignement of the return values from the accept function
-    clientsocket, address = sock.accept()
-    print(f"Connection from {address} has been established!")
+class client2Class:
     
-    msg = "Welcome from client 1!"
-    # msg = input("send msg to client: ")
-    msg = f"{len(msg):<{HEADERSIZE}}" + msg
-    
-    clientsocket.send(bytes(msg, "utf-8"))
-   
-    while True:
-        msg = input("send another msg to client 1: ")
-        msg = f"{len(msg):<{HEADERSIZE}}" + msg
-        clientsocket.send(bytes(msg, "utf-8"))
-        # msg = "Welcome from client 1!"
+    HEADERSIZE = 10
+    IP = socket.gethostbyname(socket.gethostname())
+    PORT = 1234
 
 
+    def __init__(self):    
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((client2Class.IP, client2Class.PORT))
+        # print(socket.gethostbyname(socket.gethostname()))  - print out current IP
+        self.sock.listen(3)
+        # sock.close shutdown
 
 
-# print(msg, "dsadsfdf")
-# sending a message which includes the length of the message and
-# putting 10 characters (Headersize) in front (includes the length of the message)
-# e.g.: 22        Welcome from client 1! dsadsfdf
-# run into problems when the message length has more then 10 digits
+    def main(self):
+        print("---Client 1 active---")
+        while True:
+            # double assignement of the return values from the accept function
+            conn, address = self.sock.accept()
+            print(f"Connection from {address} has been established!")
+            msg = "Welcome from client 1!"
+            msg = f"{len(msg):<{client2Class.HEADERSIZE}}" + msg
+            
+            conn.send(bytes(msg, "utf-8"))
+
+            while True:
+                msg = input("send another msg to client 2: ")
+                msg = f"{len(msg):<{client2Class.HEADERSIZE}}" + msg
+                conn.send(bytes(msg, "utf-8"))
+
+  
+client2Object = client2Class()
+client2Object.main()
