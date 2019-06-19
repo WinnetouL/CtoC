fdd21387d3958d1657efccaa95bf64a7ffb14c86
T3 <50875993+WinnetouL@users.noreply.github.com>
2019-06-19 20:50:42 +0200

    ver 6: PEP8 compliant (flake8 / black); comments should be done differently, but I go with them anyway

--- Cli-1.py
+++ Cli-1.py
@@ -1,71 +1,78 @@
-# dynamic chatting
-# all within defined functions / classes (modular structure)
-# just one socket in use
-# prevent application from crashing due an expected 'ConnectionResetError' (connection handling) and restarting
+# + dynamic chatting
+# + all within defined functions / classes (modular structure)
+# + just one socket in use
+# + prevent application from crashing due an expected "ConnectionResetError"
+# (connection handling) and restarting
 
-import sys
+
 import time
 import socket
 import threading
 
 
-
 class client1Class:
-    # class variabels:
-    TCP_IP = socket.gethostbyname(socket.gethostname())                     # TCP_IP = 'XXX.XXX.XXX.XXX'
+    TCP_IP = socket.gethostbyname(socket.gethostname())
     TCP_PORT = 1234
     HEADERSIZE = 10
 
-    # initalize socket
     def __init__(self):
         print("---- Client 1 active - waiting for connections ----\n")
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       # AF_INET = IPv4 | SOCK_STREAM = TCP
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.bind((client1Class.TCP_IP, client1Class.TCP_PORT))
-        self.sock.listen(1)                                                 # "number of unaccepted connections that the system will allow before refusing new connections"
-        self.conn, self.addr = self.sock.accept()                           # the accept funtion returns 2 values, which get assigned to variable
-        
+        self.sock.listen(1)
+        self.conn, self.addr = self.sock.accept()
 
     def main(self):
-        print('Connection from: client-1 ', socket.gethostbyname(socket.gethostname()), ' to CLIENT-2: ', self.addr[0])      # instead of "self.addr" I could just use TCP_IP
-    
-        t0 = threading.Thread(target = self.recvCli1)
-        t1 = threading.Thread(target = self.sendCli1)
+        print(
+            "Connection from: client-1 ",
+            socket.gethostbyname(socket.gethostname()),
+            " to CLIENT-2: ",
+            self.addr[0],
+        )
+        t0 = threading.Thread(target=self.recvCli1)
+        t1 = threading.Thread(target=self.sendCli1)
         while True:
-            if t0.is_alive() == False:
-                t0 = threading.Thread(target = self.recvCli1)
-                t0.daemon = True                                            # mark these functiones as daemon threads, which are parts of the main thread (main > daemon).
-                t0.start()  
-            elif t1.is_alive() == False:
-                t1 = threading.Thread(target = self.sendCli1)
-                t1.daemon = True                                            # This enables the possibility to stop the main thread with cmd + c and the other threads will stop as well, because they are daemons. Otherwise the started threads would continuously run.
+            if t0.is_alive() is False:
+                t0 = threading.Thread(target=self.recvCli1)
+                t0.daemon = True
+                t0.start()
+            elif t1.is_alive() is False:
+                t1 = threading.Thread(target=self.sendCli1)
+                t1.daemon = True
                 t1.start()
             else:
-                time.sleep(2)                                               # a = threading.active_count() | print("Threads alive: ", a)
+                time.sleep(2)
                 pass
-                
-        
+
     def recvCli1(self):
         try:
             while True:
-                fullClient2Msg = ''
+                fullClient2Msg = ""
                 newClient2Msg = True
-                
                 while True:
-                    msg = self.conn.recv(16)                                # 'print(self.conn)' to see that the raddr port changes
-                    if newClient2Msg == True:                               # the if part will be passed just once to figure out how long the msg will be, because newClientRespond is going to be set to false
-                        print(f"First 10 characters of Client-2's message: {msg[:client1Class.HEADERSIZE]}")       # printing out the first 10 characters (Headersize) of the message
-                        msgLen = int(msg[:client1Class.HEADERSIZE])         # append everything of the first 10 characters (Headersize) to 'msgLen'. In our case it is always just the number of the length and blanks nothing else, so e.g.: 22
+                    msg = self.conn.recv(16)
+                    if newClient2Msg is True:
+                        print(
+                            f"First 10 characters of Client-2's message:\
+                            {msg[:client1Class.HEADERSIZE]}"
+                        )
+                        msgLen = int(msg[: client1Class.HEADERSIZE])
                         newClient2Msg = False
-                    
-                    fullClient2Msg += msg.decode("utf-8")                   # decode the chunks of the received msg by given transformation format and append 16 characters each turn of the while loop to the full msg variable ('fullClient2Msg')
-                    
-                    if len(fullClient2Msg)-client1Class.HEADERSIZE == msgLen:                                               # this part is only going to be passed if the (length of 'fullClient2Msg' (1.round=16, 2.round=32 (next 16 'or less'))) - ('Headersize' (10 characters)) equals the determined ('msgLen' (22))
-                        print("Client-2's message received: ", fullClient2Msg[client1Class.HEADERSIZE:])                    # if this is true full message is received. Printing everything out continuing after the 10 characters (Headersize)
+                    fullClient2Msg += msg.decode("utf-8")
+                    if len(fullClient2Msg) - client1Class.HEADERSIZE == msgLen:
+                        print(
+                            "Client-2's message received: ",
+                            fullClient2Msg[client1Class.HEADERSIZE :],
+                        )
                         break
         except ConnectionResetError as e1:
-            print("Client-2 closed the window\n", "OS-Error:", e1, "\nApplication restarted")
-            self.__init__()                                                 # due interruption of the connection (ConnectionResetError "raised when a connection is reset by the peer"), I suppose I need to run my socket setup function again, which causes a port (raddr) change - just run main() again doesn't work.
-
+            print(
+                "Client-2 closed the window\n",
+                "OS-Error:",
+                e1,
+                "\nApplication restarted",
+            )
+            self.__init__()
 
     def sendCli1(self):
         while True:
@@ -74,7 +81,54 @@ class client1Class:
             self.conn.send(bytes(msg, "utf-8"))
 
 
-
-
 client1Object = client1Class()
 client1Object.main()
+
+
+"""Line 14: IP can be set here.
+
+Line 20: AF_INET = IPv4 | SOCK_STREAM = TCP
+
+Line 22: "Number of unaccepted connections that the system will allow before
+refusing new connections."
+
+Line 23: The accept funtion returns 2 values, which get assigned to variable.
+
+Line 30: Instead of "self.addr" I could just use TCP_IP.
+
+Line 37: Mark these functiones as daemon threads, which are parts of the main
+thread (main > daemon).
+
+Line 41: This enables the possibility to stop the main thread with cmd + c and
+the other threads will stop as well, because they are daemons.
+Otherwise the started threads would continuously run.
+
+Line 44: a = threading.active_count() | print("Threads alive: ", a)
+
+Line 53: "print(self.conn)" to see that the raddr port changes
+
+Line 54: The if-part will be passed just once to figure out how long the msg
+will be, because newClientRespond is going to be set to false.
+
+Line 55: Printing out the first 10 characters (Headersize) of the message.
+
+Line 59: Append everything of the first 10 characters (Headersize) to "msgLen".
+In our case it is always just the number of the length and blanks nothing else,
+so e.g.: 22.
+
+Line 61: Decode the chunks of the received msg by given transformation format
+and append 16 characters for each turn of the while loop to
+the full msg variable ("fullClient2Msg").
+
+Line 62: This part is only going to be passed
+if the (length of "fullClient2Msg" (1.round=16, 2.=32 (next 16 "or less")))
+- ("Headersize" (10 characters)) equals the determined ("msgLen" (22)).
+
+Line 63: If this is true full message is received. Printing everything into
+stdout after 10 characters (Headersize) onwards.
+
+Line 75: Due interruption of the connection (ConnectionResetError "raised when
+a connection is reset by the peer"),
+I suppose I need to run my socket setup function again,
+which causes a port (raddr) change - just run main() again doesn't work.
+"""
--- Cli-2.py
+++ Cli-2.py
@@ -1,7 +1,9 @@
-# dynamic chatting
-# all within defined functions / classes (modular structure)
-# just one socket in use
-# prevent application from crashing due an expected 'ConnectionResetError' (connection handling) and quitting
+# + dynamic chatting
+# + all within defined functions / classes (modular structure)
+# + just one socket in use
+# + prevent application from crashing due an expected "ConnectionResetError"
+# (connection handling) and quitting
+
 
 import sys
 import time
@@ -9,30 +11,29 @@ import socket
 import threading
 
 
-
 class client2Class:
-    # class variabels:
-    TCP_IP = socket.gethostbyname(socket.gethostname())                     # TCP_IP = 'XXX.XXX.XXX.XXX'
+    TCP_IP = socket.gethostbyname(socket.gethostname())
     TCP_PORT = 1234
     HEADERSIZE = 10
 
-    # connect to the default port
     def __init__(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       # AF_INET = IPv4 | SOCK_STREAM = TCP
-        self.sock.connect((client2Class.TCP_IP, client2Class.TCP_PORT))     # connect to client socket
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((client2Class.TCP_IP, client2Class.TCP_PORT))
         print("---- Client 2 active ----\n")
 
-
     def main(self):
-        print('Connection from client-2: ', socket.gethostbyname(socket.gethostname()), ' to CLIENT-1: ', client2Class.TCP_IP)
-        
-        t0 = threading.Thread(target = self.sendCli2)
-        t1 = threading.Thread(target = self.recvCli2)
-        t0.daemon = True                                                    # mark these functiones as daemon threads, which are parts of the main thread (main > daemon).
-        t1.daemon = True                                                    # This enables the possibility to stop the main thread with cmd + c and the other threads will stop as well, because they are daemons. Otherwise the started threads would continuously run.
+        print(
+            "Connection from client-2: ",
+            socket.gethostbyname(socket.gethostname()),
+            " to CLIENT-1: ",
+            client2Class.TCP_IP,
+        )
+        t0 = threading.Thread(target=self.sendCli2)
+        t1 = threading.Thread(target=self.recvCli2)
+        t0.daemon = True
+        t1.daemon = True
         t0.start()
         t1.start()
-
         while True:
             a = threading.active_count()
             if a != 3:
@@ -43,38 +44,60 @@ class client2Class:
             else:
                 time.sleep(2)
                 pass
-                
 
     def sendCli2(self):
         while True:
             msg = input("Second msg to client-1?: ")
-            msg = f"{len(msg):<{client2Class.HEADERSIZE}}" + msg            # rebuild msg: counting length of msg and append the length number in front of the msg within the defined headersize
-            self.sock.send(bytes(msg, "utf-8"))                             # send msg in given transformation format
-        
+            msg = f"{len(msg):<{client2Class.HEADERSIZE}}" + msg
+            self.sock.send(bytes(msg, "utf-8"))
 
     def recvCli2(self):
         try:
             while True:
-                fullClient1Msg = ''
+                fullClient1Msg = ""
                 newClient1Msg = True
-
                 while True:
                     msg = self.sock.recv(16)
-                    if newClient1Msg == True:
-                        print(f"First 10 characters of client-1's message: {msg[:client2Class.HEADERSIZE]}")
-                        msgLen = int(msg[:client2Class.HEADERSIZE])
+                    if newClient1Msg is True:
+                        print(
+                            f"First 10 characters of client-1's message:\
+                            {msg[:client2Class.HEADERSIZE]}"
+                        )
+                        msgLen = int(msg[: client2Class.HEADERSIZE])
                         newClient1Msg = False
-
                     fullClient1Msg += msg.decode("utf-8")
-
-                    if len(fullClient1Msg)-client2Class.HEADERSIZE == msgLen:
-                        print("Client-1's message received: ", fullClient1Msg[client2Class.HEADERSIZE:])
+                    if len(fullClient1Msg) - client2Class.HEADERSIZE == msgLen:
+                        print(
+                            "Client-1's message received: ",
+                            fullClient1Msg[client2Class.HEADERSIZE :],
+                        )
                         break
         except ConnectionResetError as e:
-            print("Client-1 closed the window\n", "OS-Error:", e, "\nApplication quitted")
-            
-            
+            print(
+                "Client-1 closed the window\n",
+                "OS-Error:",
+                e,
+                "\nApplication quitted",
+            )
 
 
 client2Object = client2Class()
 client2Object.main()
+
+
+"""Line 15: IP can be set here.
+
+Line 20: AF_INET = IPv4 | SOCK_STREAM = TCP
+
+Line 33: Mark these functiones as daemon threads, which are parts of the main
+thread (main > daemon).
+
+Line 34: This enables the possibility to stop the main thread with cmd + c and
+the other threads will stop as well, because they are daemons.
+Otherwise the started threads would continuously run.
+
+Line 51: Rebuild msg: Counting length of msg and
+append the length number in front of the msg within the defined headersize.
+
+Line 52: Send msg in given transformation format.
+"""
