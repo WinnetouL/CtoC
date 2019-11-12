c9f14ae96ec8a0e60e7068a1e59a4229c4de61d5
T3 <50875993+WinnetouL@users.noreply.github.com>
2019-11-12 23:23:22 +0100

    v2 PEP compliance changes

--- client.py
+++ client.py
@@ -6,20 +6,20 @@ import socket
 import threading
 
 
-class clientClass:
+class ClientClass:
     TCP_IP = socket.gethostbyname(socket.gethostname())
     TCP_PORT = 1234
     HEADERSIZE = 20
     USERNAME = ""
-    LOCK = threading.Lock()
 
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((clientClass.TCP_IP, clientClass.TCP_PORT))
+        self.sock.connect((ClientClass.TCP_IP, ClientClass.TCP_PORT))
+        self.clientLock = threading.Lock()
         print("---- Client active ----\n")
 
     def main(self):
-        print("Connection from client: ", socket.gethostbyname(socket.gethostname()), " to server: ", clientClass.TCP_IP, "\n")
+        print("Connection from client: ", socket.gethostbyname(socket.gethostname()), " to server: ", ClientClass.TCP_IP, "\n")
         t0 = threading.Thread(target=self.send, args=("{name}",))
         t0.start()
         t1 = threading.Thread(target=self.recv)
@@ -28,34 +28,34 @@ class clientClass:
         t2 = threading.Thread(target=self.send, args=("{switch}",))
         t2.start()
         while True:
-            if t2.isAlive() is False:
-                msgOrTypeOfMsg = input(f"<{clientClass.USERNAME}> ({{switch}}/{{quit}})")
+            if not t2.isAlive():
+                msgOrTypeOfMsg = input(f"<{ClientClass.USERNAME}> ({{switch}}/{{quit}})")
                 t3 = threading.Thread(target=self.send, args=(msgOrTypeOfMsg,))
                 t3.start()
             else:
                 time.sleep(2)
                 pass
 
-    def send(self, msgOrTypeOfMsg="!"):
-        with clientClass.LOCK:
+    def send(self, msgOrTypeOfMsg=None):
+        with self.clientLock:
             if msgOrTypeOfMsg == "{switch}":
                 switchPrefix = msgOrTypeOfMsg + str(len(msgOrTypeOfMsg.encode("utf-8")))
-                msg = f"{switchPrefix:<{clientClass.HEADERSIZE}}" + msgOrTypeOfMsg
+                msg = f"{switchPrefix:<{ClientClass.HEADERSIZE}}" + msgOrTypeOfMsg
                 self.sock.send(bytes(msg, "utf-8"))
                 destination = input("Send to?: ")
                 switchPrefix = msgOrTypeOfMsg + str(len(destination.encode("utf-8")))
-                msg = f"{switchPrefix:<{clientClass.HEADERSIZE}}" + destination
+                msg = f"{switchPrefix:<{ClientClass.HEADERSIZE}}" + destination
                 self.sock.send(bytes(msg, "utf-8"))
             elif msgOrTypeOfMsg == "{quit}":
                 print(msgOrTypeOfMsg, "//quit")
             elif msgOrTypeOfMsg == "{name}":
-                clientClass.USERNAME = input("Username ? ")
-                print(f"--> You chose {clientClass.USERNAME}\n")
-                userNamePrefix = msgOrTypeOfMsg + str(len(clientClass.USERNAME.encode("utf-8")))
-                msg = f"{userNamePrefix:<{clientClass.HEADERSIZE}}" + clientClass.USERNAME
+                ClientClass.USERNAME = input("Username ? ")
+                print(f"--> You chose {ClientClass.USERNAME}\n")
+                userNamePrefix = msgOrTypeOfMsg + str(len(ClientClass.USERNAME.encode("utf-8")))
+                msg = f"{userNamePrefix:<{ClientClass.HEADERSIZE}}" + ClientClass.USERNAME
                 self.sock.send(bytes(msg, "utf-8"))
             else:
-                msg = f"{str(len(msgOrTypeOfMsg.encode('utf-8'))):<{clientClass.HEADERSIZE}}" + msgOrTypeOfMsg
+                msg = f"{str(len(msgOrTypeOfMsg.encode('utf-8'))):<{ClientClass.HEADERSIZE}}" + msgOrTypeOfMsg
                 self.sock.send(bytes(msg, "utf-8"))
 
     def recv(self):
@@ -65,17 +65,17 @@ class clientClass:
                 newServerMsg = True
                 while True:
                     msg = self.sock.recv(32)
-                    if newServerMsg is True:
-                        msgLen = int(msg[: clientClass.HEADERSIZE])
+                    if newServerMsg:
+                        msgLen = int(msg[: ClientClass.HEADERSIZE])
                         newServerMsg = False
                     fullServerMsg += msg
-                    if len(fullServerMsg) - clientClass.HEADERSIZE == msgLen:
+                    if len(fullServerMsg) - ClientClass.HEADERSIZE == msgLen:
                         fullServerMsg = fullServerMsg.decode("utf-8")
-                        print(f"\n{fullServerMsg[clientClass.HEADERSIZE :]}")
+                        print(f"\n{fullServerMsg[ClientClass.HEADERSIZE :]}")
                         break
         except ConnectionResetError as e:
             print("Server closed the connection\n", "OS-Error:", e, "\nApplication quitted")
 
 
-clientObject = clientClass()
+clientObject = ClientClass()
 clientObject.main()
